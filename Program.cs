6d9eb2758f9cbfa3bd8ff742b86885fbaa4f1f47
LebbonJ <justin.lebbon@students.snow.edu>
2025-02-12 14:00:33 -0700

    Step 2 Write a Greeting

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// Justin Lebbon February 12, 2025
 //Lab 3 - Mastermind
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Hello, and welcome to your favorite guessing game!");
+Console.Write("Press any key to continue...");
+Console.ReadKey();
+Console.WriteLine("I have chosen 4 letters between 'a' and 'g' and have arranged them in a particular order.");
+Console.WriteLine("Your job is to guess the letters and put them in the right order.");
+
