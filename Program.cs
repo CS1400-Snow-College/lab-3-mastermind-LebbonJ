aefc30e82ba509d590b736c693f35e5dab386964
LebbonJ <justin.lebbon@students.snow.edu>
2025-02-12 16:10:43 -0700

    Step 3 Make do/while loop

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿// Justin Lebbon February 12, 2025
 //Lab 3 - Mastermind
 Console.Clear();
-Console.WriteLine("Hello, and welcome to your favorite guessing game!");
+Console.WriteLine("Hello, and Welcome to your favorite guessing game!");
 Console.Write("Press any key to continue...");
 Console.ReadKey();
 Console.WriteLine("I have chosen 4 letters between 'a' and 'g' and have arranged them in a particular order.");
 Console.WriteLine("Your job is to guess the letters and put them in the right order.");
+
+string answerKey = "bcfe";
+string guess = "";
+
+int numGuess = 0;
+
+do
+{   
+    numGuess++;
+
+    Console.WriteLine($"Guess #{numGuess} :Please guess a sequence of 4 lowercase letters with no repeats.");
+    guess = Console.ReadLine();
+
+    
+
+}
+while (guess != "bcfe");
 
--- obj/Debug/net8.0/lab-3-mastermind-LebbonJ.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-3-mastermind-LebbonJ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-3-mastermind-LebbonJ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06731faf0908a8d7e7545330e4bdea50133df04a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9eb2758f9cbfa3bd8ff742b86885fbaa4f1f47")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-3-mastermind-LebbonJ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-3-mastermind-LebbonJ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
