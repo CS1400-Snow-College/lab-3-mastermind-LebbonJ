28123eae9d6b1c0d447d778437d8bd85695f6ca1
LebbonJ <justin.lebbon@students.snow.edu>
2025-02-12 20:08:35 -0700

    Step 5 make nested loop to check for correct letters

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ string answerKey = "bcfe";
 string guess = "";
 
 int numGuess = 0;
+int matchlettercount = 0;  // Count of letters in the correct position
+
 
 do
 {   
@@ -19,8 +21,17 @@ do
     Console.WriteLine($"Guess #{numGuess} :Please guess a sequence of 4 lowercase letters with no repeats.");
     guess = Console.ReadLine();
 
-    
+        for (int i = 0; i < answerKey.Length; i++)
+        {
+            if (guess[i] == answerKey[i])
+            {
+                matchlettercount++;
+            }
+           
+        }
 
+    Console.WriteLine($" {matchlettercount} - correctly placed letters");
+    
 }
 while (guess != "bcfe");
 
